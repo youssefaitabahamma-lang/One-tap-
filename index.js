6ddccd485a4db49fac1bdb50fb01c1988bf152e8
youssefaitabahamma-lang <youssefaitabahamma@gmail.com>
2026-07-23 23:31:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const {
   Client,
+  Collection,
   GatewayIntentBits
 } = require("discord.js");
+
+const fs = require("fs");
 
 const client = new Client({
   intents: [
@@ -10,8 +13,33 @@ const client = new Client({
   ]
 });
 
-client.once("ready", () => {
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once("ready", async () => {
   console.log(`${client.user.tag} is online!`);
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 client.login(process.env.TOKEN);
