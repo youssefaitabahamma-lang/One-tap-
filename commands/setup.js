d4ccd61acf5310bb960e113588c19eae3646089e
youssefaitabahamma-lang <youssefaitabahamma@gmail.com>
2026-07-23 23:42:37 +0100

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -1,1 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("setup")
+    .setDescription("Setup the One Tap voice system"),
+
+  async execute(interaction) {
+    await interaction.reply({
+      content: "✅ One Tap setup completed!",
+      ephemeral: true,
+    });
+  },
+};
